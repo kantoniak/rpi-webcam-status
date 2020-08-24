4eadc65ecd7dc860b12f1975f1b289c13cf29a4c
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-08-24 20:47:14 +0200

    Synchronize calls to DiodeHandler

--- rpi/diode_handler.py
+++ rpi/diode_handler.py
@@ -1,25 +1,44 @@
 import RPi.GPIO as GPIO
+from wrapt.decorators import synchronized
 
 class DiodeHandler:
 
     def __init__(self, green_pin, red_pin):
+        self.running = False
         self.green_pin = green_pin
         self.red_pin = red_pin
+        self.last_update = None
 
+    @synchronized
     def start(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(self.green_pin, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.setup(self.red_pin, GPIO.OUT, initial=GPIO.HIGH)
+        if not self.running:
+            self.running = True
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(False)
+            GPIO.setup(self.green_pin, GPIO.OUT, initial=GPIO.HIGH)
+            GPIO.setup(self.red_pin, GPIO.OUT, initial=GPIO.HIGH)
     
+    @synchronized
     def stop(self):
-        GPIO.output(self.green_pin, GPIO.LOW)
-        GPIO.output(self.red_pin, GPIO.LOW)
+        if self.running:
+            self.running = False
+            GPIO.output(self.green_pin, GPIO.LOW)
+            GPIO.output(self.red_pin, GPIO.LOW)
     
+    @synchronized
+    def set_all(self):
+        if self.running:
+            GPIO.output(self.green_pin, GPIO.HIGH)
+            GPIO.output(self.red_pin, GPIO.HIGH)
+
+    @synchronized
     def set_green(self):
-        GPIO.output(self.green_pin, GPIO.HIGH)
-        GPIO.output(self.red_pin, GPIO.LOW)
+        if self.running:
+            GPIO.output(self.green_pin, GPIO.HIGH)
+            GPIO.output(self.red_pin, GPIO.LOW)
     
+    @synchronized
     def set_red(self):
-        GPIO.output(self.green_pin, GPIO.LOW)
-        GPIO.output(self.red_pin, GPIO.HIGH)
+        if self.running:
+            GPIO.output(self.green_pin, GPIO.LOW)
+            GPIO.output(self.red_pin, GPIO.HIGH)
